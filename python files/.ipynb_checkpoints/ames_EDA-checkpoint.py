9d18ac1e31640f9835e265482f62db80ed21440f
Yuan Wang <sofiawang2401@gmail.com>
2024-04-18 14:08:52 -0400

    updates

--- python files/.ipynb_checkpoints/ames_EDA-checkpoint.py
+++ python files/.ipynb_checkpoints/ames_EDA-checkpoint.py
@@ -4,7 +4,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
+import plotly.io as pio
 from io import StringIO
+pio.templates.default='seaborn'
+
 # Add title
 st.title("EDA with Streamlit")
 # Define the columns you want to use 
@@ -129,4 +133,80 @@ st.markdown("#### Displaying appropriate plot based on selected column")
 # Display appropriate eda plots
 st.pyplot(fig)
 
+
+# Use plotly for explore functions
+def plotly_explore_numeric(df, x):
+    fig = px.histogram(df,x=x,marginal='box',title=f'Distribution of {x}', 
+                      width=1000, height=500)
+    return fig
+def plotly_explore_categorical(df, x):
+    fig = px.histogram(df,x=x,color=x,title=f'Distribution of {x}', 
+                          width=1000, height=500)
+    fig.update_layout(showlegend=False)
+    return fig
+# Conditional statement to determine which function to use
+if df[column].dtype == 'object':
+    fig = plotly_explore_categorical(df, column)
+else:
+    fig = plotly_explore_numeric(df, column)
+    
+st.markdown("#### Displaying appropriate Plotly plot based on selected column")
+# Display appropriate eda plots
+st.plotly_chart(fig)
+
+
+
+
+
+def plot_hist(df, column):
+    fig, ax = plt.subplots()
+    sns.histplot(data = df, x = column)
+    return fig
+
+# Test the function
+fig = plot_hist(df, 'SalePrice')
+
+st.markdown("#### Explore Features vs. Sale Price with Plotly")
+# Add a selectbox for all possible features (exclude SalePrice)
+# Copy list of columns
+features_to_use = columns_to_use[:]
+# Define target
+target = 'SalePrice'
+# Remove target from list of features
+features_to_use.remove(target)
+
+# Add a selectbox for all possible columns
+feature = st.selectbox(label="Select a feature to compare with Sale Price", options=features_to_use)
+
+
+
+def plotly_numeric_vs_target(df, x, y='SalePrice', trendline='ols',add_hoverdata=True):
+    if add_hoverdata == True:
+        hover_data = list(df.columns)
+    else: 
+        hover_data = None
+        
+    pfig = px.scatter(df, x=x, y=y,width=800, height=600,
+                     hover_data=hover_data,
+                      trendline=trendline,
+                      trendline_color_override='red',
+                     title=f"{x} vs. {y}")
+    
+    pfig.update_traces(marker=dict(size=3),
+                      line=dict(dash='dash'))
+    return pfig
+
+def plotly_categorical_vs_target(df, x, y='SalePrice', histfunc='avg', width=800,height=500):
+    fig = px.histogram(df, x=x,y=y, color=x, width=width, height=height,
+                       histfunc=histfunc, title=f'Compare {histfunc.title()} {y} by {x}')
+    fig.update_layout(showlegend=False)
+    return fig
+
+# Conditional statement to determine which function to use
+if df[feature].dtype == 'object':
+    fig_vs  = plotly_categorical_vs_target(df, x = feature)
+else:
+    fig_vs  = plotly_numeric_vs_target(df, x = feature)
+
+st.plotly_chart(fig_vs)
 
